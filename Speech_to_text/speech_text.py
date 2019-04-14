7887fb8527bfd12eeaa2804337b6e68b360f11a1
Kevin Nguyen <mrkevin@Kevins-MacBook-Pro.local>
2019-04-13 19:55:04 -0500

    API Works

--- Speech_to_text/speech_text.py
+++ Speech_to_text/speech_text.py
@@ -1,33 +1,58 @@
-#!/usr/bin/env python3
-import io
-import os
+#!/usr/bin/env python
 
-# Imports the Google Cloud client library
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Instantiates a client
-client = speech.SpeechClient()
 
-# The name of the audio file to transcribe
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
+def run_quickstart():
+    # [START speech_quickstart]
+    import io
+    import os
 
-# Loads the audio into memory
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+    # Imports the Google Cloud client library
+    # [START speech_python_migration_imports]
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    # [END speech_python_migration_imports]
 
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
+    # Instantiates a clients
+    # [START speech_python_migration_client]
+    client = speech.SpeechClient()
+    # [END speech_python_migration_client]
 
-# Detects speech in the audio file
-response = client.recognize(config, audio)
+    # The name of the audio file to transcribe
 
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'motivation.flac')
+    # Loads the audio into memory
+    #file_name = 'Obama.mp3'
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        #sample_rate_hertz=16000,
+        language_code='en-US')
+
+    # Detects speech in the audio file
+    response = client.recognize(config, audio)
+    print(response)
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+    # [END speech_quickstart]
+if __name__ == '__main__':
+    run_quickstart()
